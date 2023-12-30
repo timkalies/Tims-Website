ac784238ce53c5f8f54c1ba7b00da933c85594a2
TIM_KALIES <kaliestim@gmail.com>
2023-12-30 08:00:18 +0100

    TypeError durch if-Abfrage gelöst. qparts benötigt Umfragewerte in URL. So läuft der Cpde nur wenn das der Fall ist

--- kontakt.js
+++ kontakt.js
@@ -1,19 +1,24 @@
-var select = [];
-select = getValue();
+let url = window.location.href;
+let qparts = url.split("?");
 
-function getValue() {
+// Überprüfe, ob Query-Parameter vorhanden sind
+if (qparts.length > 1) {
+    let inputparts = qparts[1].split("&");
 
-var url = window.location.href;
-var qparts = url.split("?");
-var inputparts = qparts[1].split("&");
-var nameparts = inputparts[0].split("=");
-var emailparts = inputparts[1].split("=");
-var textparts = inputparts[2].split("=");
+    let nameparts = inputparts[0].split("=");
+    let emailparts = inputparts[1].split("=");
+    let textparts = inputparts[2].split("=");
 
-if (nameparts[1] == '') {
-  nameparts[1] = 'Kein Name eingegeben';
-}
-return [nameparts[1], emailparts[1], textparts[1]];
+    if (nameparts[1] == '') {
+        nameparts[1] = 'Kein Name eingegeben';
+    }
 
+    let select = [nameparts[1], emailparts[1], textparts[1]];
 
+    // Der Rest deines Codes, falls vorhanden...
+
+    console.log(select); // Beispiel-Ausgabe der extrahierten Werte
+} else {
+    // Behandlung für den Fall, dass keine Query-Parameter vorhanden sind.
+    console.error('Keine Query-Parameter in der URL gefunden.');
 }
