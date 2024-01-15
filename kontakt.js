bc20e35177774e73179bafe94bff1f9a0977306c
TIM_KALIES <kaliestim@gmail.com>
2024-01-15 13:33:35 +0100

    Kontaktseiten problem gefixt, dass Input nicht angezeigt wurde

--- kontakt.js
+++ kontakt.js
@@ -3,23 +3,24 @@ let qparts = url.split("?");
 
 // Überprüfe, ob Query-Parameter vorhanden sind
 if (qparts.length > 1) {
+    let queryParams = {};
     let inputparts = qparts[1].split("&");
 
-    let nameparts = inputparts[0].split("=");
-    let emailparts = inputparts[1].split("=");
-    let textparts = inputparts[2].split("=");
+    inputparts.forEach(part => {
+        let [key, value] = part.split("=");
+        queryParams[key] = value || 'Kein Wert eingegeben';
+    });
 
-    if (nameparts[1] == '') {
-        nameparts[1] = 'Kein Name eingegeben';
-    }
-
-    let select = [nameparts[1], emailparts[1], textparts[1]];
+    let select = [queryParams.name, queryParams.email, queryParams.text];
 
     // Der Rest deines Codes, falls vorhanden...
 
-    console.log(select); // Beispiel-Ausgabe der extrahierten Werte
+    console.log(select);
+
+    // Hier setzt du die Query-Parameter für die spätere Verwendung
+    window.select = select;
 } else {
     // Behandlung für den Fall, dass keine Query-Parameter vorhanden sind.
     console.error('Keine Query-Parameter in der URL gefunden.');
-    return ['Kein Name eingegeben', '', ''];
+    window.select = ['Kein Name eingegeben', '', ''];
 }
