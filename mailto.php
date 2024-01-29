f7d26de6518656b6a5f293b19b847fe88dfec688
TIM_KALIES <kaliestim@gmail.com>
2024-01-29 21:13:07 +0100

    Fehlerbehebungen

--- mailto.php
+++ mailto.php
@@ -17,7 +17,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   // E-Mail-Inhalt
   $nachricht = "Name: $name\n";
   $nachricht .= "E-Mail: $email\n";
-  $nachricht = "Betreff: $info\n";
+  $nachricht .= "Betreff: $info\n";
   $nachricht .= "Text: $text\n";
 
   // Weitere Formulardaten hier einfügen
