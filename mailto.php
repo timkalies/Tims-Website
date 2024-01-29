c0aaa1b89d5c736caeb2c87f888b1f9387d8dadc
TIM_KALIES <kaliestim@gmail.com>
2024-01-29 20:58:56 +0100

    kontaktformular funktioniert

--- mailto.php
+++ mailto.php
@@ -1,11 +1,30 @@
 <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  // Empfänger-E-Mail-Adresse
+  $empfaenger = "kontakt@timkalies.de";
 
-if($_POST["message"]) {
+  // Betreff der E-Mail
+  $betreff = "Neue Kontaktanfrage von $name";
 
-mail("kontakt@timkalies.de", "Kontakt",
+  // Daten aus dem Formular
+  $name = $_POST["name"];
+  $email = $_POST["email"];
+  $name = $_POST["betreff"];
+  $email = $_POST["text"];
 
-$_POST["insert your message here"]. "From: an@email.address");
+  // Weitere Formulardaten hier einfügen
 
+  // E-Mail-Inhalt
+  $nachricht = "Name: $name\n";
+  $nachricht .= "E-Mail: $email\n";
+  $nachricht = "Betreff: $betreff\n";
+  $nachricht .= "Text: $text\n";
+
+  // Weitere Formulardaten hier einfügen
+
+  // E-Mail senden
+  mail($empfaenger, $betreff, $nachricht);
+
+  exit();
 }
-
 ?>
