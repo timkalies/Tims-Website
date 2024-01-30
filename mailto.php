feb2586d56ba26fd13c7f6329d3297e4f76a650a
TIM_KALIES <kaliestim@gmail.com>
2024-01-30 19:34:00 +0100

    SEO Freundlichkeit + Kontatformular funktioniert

--- mailto.php
+++ mailto.php
@@ -1,16 +1,17 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+      // Daten aus dem Formular
+  $name = $_POST["name"];
+  $email .= $_POST["email"];
+  $info .= $_POST["betreff"];
+  $text .= $_POST["text"];
   // Empfänger-E-Mail-Adresse
   $empfaenger = "kontakt@timkalies.de";
 
   // Betreff der E-Mail
   $betreff = "Neue Kontaktanfrage von $name";
 
-  // Daten aus dem Formular
-  $name = $_POST["name"];
-  $email .= $_POST["email"];
-  $info .= $_POST["betreff"];
-  $text .= $_POST["text"];
+
 
   // Weitere Formulardaten hier einfügen
 
